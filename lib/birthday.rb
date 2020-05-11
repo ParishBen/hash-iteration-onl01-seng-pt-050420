bd73e77e185026f72d0ac38e960ad916bd0f6503
Ben Parish <parishben13@gmail.com>
2020-05-11 05:15:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
    birthday_kids.each do |kid,age|
    if age<12
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
-else false
+else "You are too old for this."
   end
  end
 end
